37dbbe0ed0f66085a8e01f894ee92be4d32883e0
Hanko0k <103627216+Hanko0k@users.noreply.github.com>
2024-05-02 17:51:51 +1200

    initial model running

--- dataset_utilities.py
+++ dataset_utilities.py
@@ -1,7 +1,10 @@
 import os
 import re
+import shutil
+from sklearn.model_selection import train_test_split
 
 metadata_folder_path = 'C:\\Users\\David\\Documents\\228798 Individual Research Project\\dataset\\Originals\\Originals'
+og_dataset = "C:\\Users\\David\\Documents\\228798 Individual Research Project\\dataset\\1 Braeburn\\1 Braeburn"
 
 def _clean_filename(name: str) -> str:
     name = os.path.splitext(name)[0]
@@ -77,8 +80,58 @@ def augment_metadata() -> None:
 
     return None
 
+def generate_file_structure(classes: list) -> None:
 
-metadata = load_metadata()
+    base_dest_dir = "braeburn binary classifier dataset"
+
+    train_dir = os.path.join(base_dest_dir, 'train')
+    val_dir = os.path.join(base_dest_dir, 'validation')
+    test_dir = os.path.join(base_dest_dir, 'test')
+
+    for dir in [train_dir, val_dir, test_dir]:
+        os.makedirs(dir, exist_ok=True)
+        for c in classes:
+            os.makedirs(os.path.join(dir, c), exist_ok=True)
+
+    img_list = [f for f in os.listdir(og_dataset) if os.path.isfile(os.path.join(og_dataset, f))]
+
+    train, temp = train_test_split(img_list, test_size=0.4, random_state=42)
+    val, test = train_test_split(temp, test_size=0.5, random_state=42)
+    
+    _move_files(train, train_dir)
+    _move_files(val, val_dir)
+    _move_files(test, test_dir)
+
+    return None
+
+def _move_files(file_names, dest_dir: str):
+    extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'}
+    for name in file_names:
+
+        _, ext = os.path.splitext(name)
+        if ext.lower() not in extensions:
+            continue
+
+        label = metadata_lookup(metadata, name, 'defect', 'braeburn')
+        shutil.move(os.path.join(og_dataset, name), os.path.join(dest_dir, label, name))
+
+def verify_directory_same_type(dir: str)-> None:
+    file_names = os.listdir(dir)
+    types_found = set()
+
+    for name in file_names:
+        types_found.add(metadata_lookup(metadata, name, 'defect', 'braeburn'))
+
+    print(f"INFO: {len(types_found)} types found in directory:")
+    for type in types_found:
+        print(type)
+
+    return None
+
+
+# metadata = load_metadata()
+# generate_file_structure(['defective', 'no defect'])
 # print(metadata.keys())
-answer = metadata_lookup(metadata, '134-03', 'defect', 'braeburn')
-print(answer)
+# answer = metadata_lookup(metadata, '002-22', 'defect', 'braeburn')
+# print(answer)
+# verify_directory_same_type("C:\\Users\\David\\Documents\\Image-Classifier-using-eXplainable-Artificial-Intelligence\\braeburn binary classifier dataset\\validation\\no defect")
